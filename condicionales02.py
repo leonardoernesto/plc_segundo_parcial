96da104f11349be19e3a39b2e27fdfe2e5f76022
leonardoernesto <145036285+leonardoernesto@users.noreply.github.com>
2023-09-27 19:21:42 -0600

    Create condicionales02.py

---
+++ condicionales02.py
@@ -0,0 +1,8 @@
+print("Camarón que se duerme: ")
+print("A. se lo lleva la corriente")
+print("B. jamás se endereza")
+respuesta = input("Tu respuesta: ")
+if respuesta == "A":
+  print("Correcto!")
+else: 
+  print("No es correcto")
